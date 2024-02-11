04fb2fbb7f90c66d7c7f7c40d17169b389890e2d
bugpiggg <bugpigg@naver.com>
2024-02-11 18:48:04 +0900

    폴더 이름 리팩토링 및 보간법, 데이터 속성 코드 추가

---
+++ 03-starting-code/app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: 'hihi',
+        }
+    },
+});
+
+app.mount('#user-goal');
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vue_complete_guide
+
 'Vue - The complete guide' lecture by Maximilian Schwarzmuller
